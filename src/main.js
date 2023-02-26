3fd9a3e66027e8ab74a21ecb3ad22338496fbfdd
Hans Naert <hans.naert@vives.be>
2023-02-26 14:17:15 +0100

    Add Vuetify initialisation

--- src/main.js
+++ src/main.js
@@ -3,4 +3,16 @@ import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,  
+})
+
+createApp(App).use(vuetify).mount('#app')
