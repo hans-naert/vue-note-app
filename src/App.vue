f3247115ec1690241120321c9ac65f4436889c34
Hans Naert <hans.naert@vives.be>
2023-04-21 19:45:07 +0200

    show Notes and add empty Note

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Notes from './components/Notes.vue';
 import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+let pages=ref([{title: 'First Note', content: 'Note Content of First Note'},{title: 'Second Note', content: 'Note Content of Second Note'}])
+
+
+function newNote(){
+    console.log('newNote');
+    pages.value.push({title: 'New Note', content: 'Note Content of New Note'});
+}
+
+function saveNote(){
+    console.log('saveNote');
+}
+
+function deleteNote(){
+    console.log('deleteNote');
+}
+
+
 </script>
 
 <template>
@@ -12,7 +32,7 @@ import TheWelcome from './components/TheWelcome.vue'
       </v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>  
-      <img alt="Vue logo" src="./assets/logo.svg">
       <HelloWorld msg="Welcome to Your Vue.js App" />      
+      <Notes  :pages="pages" @new-note="newNote"/>
   </v-app>
 </template>
