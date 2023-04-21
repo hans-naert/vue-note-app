9d077150fa8efc03284e07b03b6eff7a73bb78f7
Hans Naert <hans.naert@vives.be>
2023-04-21 19:45:07 +0200

    show Notes and add empty Note

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Notes from './components/Notes.vue';
+import { ref } from 'vue'
+
+let pages=ref([{title: 'First Note', content: 'Note Content of First Note'},{title: 'Second Note', content: 'Note Content of Second Note'}])
+
+
+function newNote(){
+    console.log('newNote');
+    pages.value.push({title: 'New Note', content: 'Note Content of New Note'});
+}
+
+function saveNote(){
+    console.log('saveNote');
+}
+
+function deleteNote(){
+    console.log('deleteNote');
+}
+
+
 </script>
 
 <template>
-  <v-app>
+  <v-app class="d-block">
     <v-toolbar app>
       <v-icon icon="mdi-arrow-left"></v-icon>
       <v-toolbar-title>
         <span>VueNoteApp</span>        
       </v-toolbar-title>
       <v-spacer></v-spacer>
-    </v-toolbar>  
-      <img alt="Vue logo" src="./assets/logo.svg">
-      <HelloWorld msg="Welcome to Your Vue.js App" />      
+    </v-toolbar>        
+      <Notes  :pages="pages" @new-note="newNote"/>
   </v-app>
 </template>
---
+++ src/components/Notes.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue'
+let name = ref('Notes');
+
+const props = defineProps({ 'pages': [], index: 0 });
+const emit = defineEmits(['new-note']);
+
+
+function newNote(event) {
+    console.log(event.target.tagName)
+    emit('new-note')
+}
+
+</script>
+
+<template>
+    <v-card class="mt-6" v-for="page in pages">
+        <v-card-title label="Note Title" outlined>{{ page.title }}  </v-card-title>
+        <v-card-text label="Note Content" outlined> {{ page.content }}</v-card-text>
+    </v-card>   
+
+    <v-card class="mt-4">
+        <v-card-title>
+            <v-text-field v-model="name" label="Note Title" outlined></v-text-field>
+        </v-card-title>
+        <v-card-text>
+            <v-textarea v-model="name" label="Note Content" outlined></v-textarea>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn  icon color="primary" size="x-large" @click="$emit('new-note')">
+                <v-icon icon="mdi-plus-circle"></v-icon>
+                Add
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn  icon color="secondary" size="x-large" @click="newNote">
+                <v-icon icon="mdi-plus-circle"></v-icon>
+                Add
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
