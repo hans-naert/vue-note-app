fbdb980defff8b35670939db598790d8ee8d2c09
Hans Naert <hans.naert@vives.be>
2023-04-22 19:59:24 +0200

    video with add and remove note logic

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Notes from './components/Notes.vue'
+import { ref } from 'vue'
+
+let pages= ref([{title:"Note Title", content:"Note Content"},{title:"Note Title2", content:"Note Content2"}])
+
+let title = ref("")
+let content = ref("")
+
+let dialog = ref(false)
+
+function addNote() {
+    console.log("addNote")
+    pages.value.push({title: title.value, content: content.value})
+    dialog.value = false
+}
+
+function deleteNote (index) {
+    console.log("deleteNote",index)
+    pages.value.splice(index,1)
+}
+
 </script>
 
 <template>
@@ -12,7 +33,43 @@ import TheWelcome from './components/TheWelcome.vue'
       </v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>  
-      <img alt="Vue logo" src="./assets/logo.svg">
-      <HelloWorld msg="Welcome to Your Vue.js App" />      
-  </v-app>
+      <Notes :pages="pages" @delete-note="deleteNote"></Notes>      
+  <v-card-actions >
+    <v-btn size="x-large"
+      color="secondary"
+      @click="dialog=true"
+    >
+    <v-icon icon="mdi-plus-circle"></v-icon>
+    Add</v-btn>
+  </v-card-actions></v-app>
+  <v-dialog v-model="dialog" max-width="500px">
+    <v-card>
+      <v-card-title class="headline">Add Note</v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="title"
+                label="Title"
+                required
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-textarea
+                v-model="content"
+                label="Content"
+                required
+              ></v-textarea>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="dialog = false">Cancel</v-btn>
+        <v-btn color="blue darken-1" text @click="addNote">Add</v-btn>
+      </v-card-actions>
+    </v-card>
+    </v-dialog>
 </template>
---
+++ src/components/Notes.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+const props = defineProps( { 'pages': []})
+const emit = defineEmits(['delete-note'])
+
+</script>
+
+<template>
+    <v-card class="my-4" v-for="(page, index) in pages">
+        <v-card-title>
+          {{ page.title }}
+        </v-card-title>
+        <v-card-text>
+            {{page.content}}
+        </v-card-text>
+        <v-card-actions>
+            <v-btn
+                color="primary"
+                @click="$emit('delete-note',index)"
+            >
+            <v-icon icon="mdi-delete"></v-icon>
+            Delete</v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
